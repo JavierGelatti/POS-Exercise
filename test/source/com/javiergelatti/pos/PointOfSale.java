fb42775c2194a9293c2e2e8f8992320bf7c7634a
Facundo Javier Gelatti <javiergelatti@gmail.com>
2016-11-30 22:24:08 -0300

    Show message when the item was not found

---
+++ test/source/com/javiergelatti/pos/PointOfSale.java
@@ -0,0 +1,11 @@
+package com.javiergelatti.pos;
+
+public class PointOfSale {
+    public void onBarcode(String barcode) {
+
+    }
+
+    public String lastTextShown() {
+        return "Item not found";
+    }
+}
--- test/source/com/javiergelatti/test/ScanProductTest.java
+++ test/source/com/javiergelatti/test/ScanProductTest.java
@@ -1,4 +1,18 @@
 package com.javiergelatti.test;
 
+import com.javiergelatti.pos.PointOfSale;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ScanProductTest {
+    @Test
+    public void itemNotFound() throws Exception {
+        PointOfSale pointOfSale = new PointOfSale();
+
+        pointOfSale.onBarcode("Not found");
+
+        String shownText = pointOfSale.lastTextShown();
+        assertEquals("Item not found", shownText);
+    }
 }
